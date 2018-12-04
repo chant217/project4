3602ede9572366a601b403d7fc45a10e998ca776
chant217 <42790620+chant217@users.noreply.github.com>
2018-12-04 18:17:43 -0500

    page 1

---
+++ maj1js/maj1.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+}
+
+// $('.slider').click(function(){
+// }
+
+
+
+//  function lightOff(){
+//  	var lightswitch = document.getElementById("lightswitch");
+//  	var lightsoff = $(".lightoff");
+//  	if (lightswitch.checked){
+//  		console.log("lightswitch is checked")
+//  		lightsoff.style.display = "block";
+//  	} else {
+//  		lightsoff.style.display = "none";
+//  	}
+//  }
+
+// lightOff();
